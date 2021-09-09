d41f7cfea4d6dc4c2edfe1e1579e20e1b8d0d1fa
Last-dreamer <asim0000.ak@gmail.com>
2021-09-10 04:58:50 +0500

    changing some code

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Chatify",
+      title: "My Chatify",
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         brightness: Brightness.dark,
